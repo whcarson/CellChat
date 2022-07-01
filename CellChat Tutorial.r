a81ccf45a9ba6d81a5d7f3e2aea0bc3f5e2e0f67
Will Carson <whcarson@uchicago.edu>
2022-07-01 11:48:34 -0700

    Changed RMD to R filetype for cross-compatibility

---
+++ CellChat Tutorial.r
@@ -0,0 +1,63 @@
+rm(list=ls())
+library(CellChat)
+library(patchwork)
+library(dplyr)
+library(Seurat)
+options(stringsAsFactors = FALSE)
+
+
+LCMA01_BEVR_CD31.data <- ReadMtx(mtx = "../Aghi Lab/CD31 sorted/LCMA01_BEVR_CD31/filtered_feature_bc_matrix/matrix.mtx.gz", features = "../Aghi Lab/CD31 sorted/LCMA01_BEVR_CD31/filtered_feature_bc_matrix/features.tsv.gz", cells = "../Aghi Lab/CD31 sorted/LCMA01_BEVR_CD31/filtered_feature_bc_matrix/barcodes.tsv.gz")
+
+LCMA01_BEVR_CD31 <- CreateSeuratObject(counts = LCMA01_BEVR_CD31.data)
+
+LCMA01_BEVR_CD31[["percent.mt"]] <- PercentageFeatureSet(LCMA01_BEVR_CD31, pattern="^MT-")
+LCMA01_BEVR_CD31 <- subset(LCMA01_BEVR_CD31, subset= nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+
+LCMA01_BEVR_CD31 <- NormalizeData(LCMA01_BEVR_CD31, normalization.method = "LogNormalize", scale.factor = 10000)
+
+LCMA01_BEVR_CD31 <- FindVariableFeatures(LCMA01_BEVR_CD31, selection.method = "vst", nfeatures=2000)
+
+all.genes <- rownames(LCMA01_BEVR_CD31)
+
+LCMA01_BEVR_CD31 <- ScaleData(LCMA01_BEVR_CD31, block.size = 1000000)
+
+LCMA01_BEVR_CD31 <- RunPCA(LCMA01_BEVR_CD31,  features=VariableFeatures(object=LCMA01_BEVR_CD31))
+print(LCMA01_BEVR_CD31[["pca"]], dims=1:15, nfeatures = 15)
+
+LCMA01_BEVR_CD31 <- FindNeighbors(LCMA01_BEVR_CD31, dims=1:10)
+LCMA01_BEVR_CD31 <- FindClusters(LCMA01_BEVR_CD31, resolution=0.5)
+
+LCMA01_BEVR_CD31 <- RunUMAP(LCMA01_BEVR_CD31, dims=1:10)
+DimPlot(LCMA01_BEVR_CD31, reduction="umap")
+
+LCMA01_BEVR_CD31_Chat <- createCellChat(object = LCMA01_BEVR_CD31, group.by="seurat_clusters")
+
+
+"Set ligand-receptor interaction db"
+CellChatDB <- CellChatDB.human
+showDatabaseCategory(CellChatDB)
+dplyr::glimpse(CellChatDB$interaction)
+
+# use a subset of CellChatDB for cell-cell communication analysis
+# CellChatDB.use <- subsetDB(CellChatDB, search = "Secreted Signaling") # use Secreted Signaling
+# use all CellChatDB for cell-cell communication analysis
+CellChatDB.use <- CellChatDB # simply use the default CellChatDB
+
+# set the used database in the object
+LCMA01_BEVR_CD31_Chat@DB <- CellChatDB.use
+
+# subset the expression data of signaling genes for saving computation cost
+LCMA01_BEVR_CD31_Chat <- subsetData(LCMA01_BEVR_CD31_Chat) # This step is necessary even if using the whole database
+future::plan("multiprocess", workers = 4) # do parallel
+LCMA01_BEVR_CD31_Chat <- identifyOverExpressedGenes(LCMA01_BEVR_CD31_Chat)
+LCMA01_BEVR_CD31_Chat <- identifyOverExpressedInteractions(LCMA01_BEVR_CD31_Chat)
+
+# project gene expression data onto PPI (Optional: when running it, USER should set `raw.use = FALSE` in the function `computeCommunProb()` in order to use the projected data)
+# cellchat <- projectData(cellchat, PPI.human)
+
+LCMA01_BEVR_CD31_Chat <- computeCommunProb(LCMA01_BEVR_CD31_Chat)
+# Filter out the cell-cell communication if there are only few number of cells in certain cell groups
+LCMA01_BEVR_CD31_Chat <- filterCommunication(LCMA01_BEVR_CD31_Chat, min.cells = 10)
+
+
+
